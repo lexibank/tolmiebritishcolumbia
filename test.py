7e4298e2d0bd19babc9b125b6f353bcc0b12d699
chrzyki <christoph@functor-argument.net>
2020-10-15 13:45:57 +0200

    Fix typo, apply markers for clicks and syllable stress correctly

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def test_valid(cldf_dataset, cldf_logger):
 
 def test_forms(cldf_dataset):
     assert len(list(cldf_dataset["FormTable"])) == 4760
-    assert any(f["Form"] == "'kha" for f in cldf_dataset["FormTable"])
+    assert any(f["Form"] == "’kha" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
